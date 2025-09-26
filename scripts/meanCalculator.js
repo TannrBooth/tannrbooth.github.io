5ddbb7b2e6222c00c8108fefab74841308bcbf77
TannrBooth <133698469+TannrBooth@users.noreply.github.com>
2025-09-26 11:28:12 -0600

    mean calculator submission

---
+++ scripts/meanCalculator.js
@@ -0,0 +1,49 @@
+let numArr = [];
+let value;
+let valueIndex;
+
+function refreshPage() {
+    let arrString = '';
+    for (num of numArr){
+        arrString += num + " ";
+    }
+    document.getElementById("dataset").innerHTML = arrString;
+
+    let currMean = 0;
+    for (num of numArr) {
+        currMean += num;
+    }
+    currMean = currMean/numArr.length;
+
+    document.getElementById("mean").innerHTML = currMean;
+}
+
+function addValue() {
+    value = parseFloat(document.getElementById("entry").value);
+    if (!value) {
+        alert("Must enter an integer");
+    }
+    else {
+        numArr.push(value);
+        refreshPage();
+    }
+    document.getElementById("entry").value = "";
+}
+
+function removeValue(){
+    value = parseFloat(document.getElementById("entry").value);
+    if (!value) {
+        alert("Must enter an integer")
+    }
+    else {
+        valueIndex = numArr.indexOf(value);
+        if (valueIndex == -1) {
+            alert("Value is not in array");
+        }
+        else {
+            numArr.splice(valueIndex,1);
+            refreshPage();
+        }
+    }
+    document.getElementById("entry").value = "";
+}
